78e284625020ae54d7db9da016a96772d1d0d712
Iris-Smok <irissmok11@gmail.com>
2022-04-11 14:10:32 +0000

    Register post and comment model, add fields property

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    for our blog content we'll use
+    summer note
+    """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Comment module
+    """
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approved_comments']
+
+    def approved_comments(self, request, queryset):
+        """
+        approval function
+        """
+        queryset.update(approved=True)
