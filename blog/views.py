de2db8dfa3b838e53ce73f559b29c1962b122ba3
Iris-Smok <irissmok11@gmail.com>
2022-04-13 16:09:27 +0000

    Add CommentForm clas to post detail view

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ views.py
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -17,21 +18,27 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
+    """
+    Post detail view
+    """
 
     def get(self, request, slug, *args, **kwargs):
+        """
+        filter post and comments
+        """
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-
         return render(
             request,
             "post_detail.html",
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
