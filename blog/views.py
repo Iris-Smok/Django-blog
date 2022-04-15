147ce7e4239446d0a670074f06f0d6795ab39005
Iris-Smok <irissmok11@gmail.com>
2022-04-15 15:43:08 +0000

    final deployment

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ views.py
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
