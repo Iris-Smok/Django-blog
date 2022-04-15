395d51f79fa1a29ff71a7092c14bc2d427848261
Iris-Smok <irissmok11@gmail.com>
2022-04-15 13:54:53 +0000

    Add new url path to PostLike view

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
