426a91e9946eea67d120d3cac1a2c75da3388e27
Iris-Smok <irissmok11@gmail.com>
2022-04-15 15:29:59 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['iris-codestar2022.herokuapp.com', 'localhost']
 
