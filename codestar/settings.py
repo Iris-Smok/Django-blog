d0cd2c387f08079102e6d29b334bd1bfba6f5a4a
Iris-Smok <irissmok11@gmail.com>
2022-04-18 10:31:54 +0000

    Error fixed

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X_FRAME_OPTIONS = 'SAME_ORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
